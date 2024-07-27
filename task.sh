fa932f0d47f8fbda94a2b08ea8489873844b96f6
elfredburger <elfredfredburger@gmail.com>
2024-07-27 13:54:49 +0200

    Solution

--- task.sh
+++ task.sh
@@ -1,8 +1,8 @@
 
-mysql -u $DB_USER -p$DB_PASSWORD -e "GRANT SELECT, LOCK TABLES, SHOW VIEW, PROCESS ON *.* TO 'backup'@'%';"
-mysql -u $DB_USER -p$DB_PASSWORD -e "GRANT ALL ON ShopDBReserve.* TO 'backup'@'%';"
-mysql -u $DB_USER -p$DB_PASSWORD -e "GRANT INSERT, LOCK TABLES, ALTER ON ShopDBDevelopment.* TO 'backup'@'%';"
-mysql -u $DB_USER -p$DB_PASSWORD -e "GRANT ALL ON *.* TO 'backup'@'%';"
+#mysql -u $DB_USER -p$DB_PASSWORD -e "GRANT SELECT, LOCK TABLES, SHOW VIEW, PROCESS ON *.* TO 'backup'@'%';"
+#mysql -u $DB_USER -p$DB_PASSWORD -e "GRANT ALL ON ShopDBReserve.* TO 'backup'@'%';"
+#mysql -u $DB_USER -p$DB_PASSWORD -e "GRANT INSERT, LOCK TABLES, ALTER ON ShopDBDevelopment.* TO 'backup'@'%';"
+
 mysqldump -u $DB_USER -p$DB_PASSWORD --databases ShopDB --result-file=full_backup.sql
 
 mysqldump -u $DB_USER -p$DB_PASSWORD --databases ShopDB --no-create-info --result-file=partial_backup.sql
