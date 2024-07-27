8abe3e14039f44afe72227a218b68b53bdc6b718
elfredburger <elfredfredburger@gmail.com>
2024-07-27 13:38:54 +0200

    Solution

--- task.sh
+++ task.sh
@@ -1,1 +1,7 @@
-#! /bin/bash
+mysqldump -u $DB_USER -p$DB_PASSWORD --databases ShopDB --result-file=full_backup.sql
+
+mysqldump -u $DB_USER -p$DB_PASSWORD --databases ShopDB --no-create-info --result-file=partial_backup.sql
+
+mysql -u $DB_USER -p$DB_PASSWORD ShopDBReserve < full_backup.sql
+
+mysql -u $DB_USER -p$DB_PASSWORD ShopDBReserve < partial_backup.sql
